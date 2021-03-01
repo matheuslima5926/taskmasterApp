6c926c14ae2021f914f19a894418123effd7612c
Matheus <matheuslima5926@gmail.com>
2021-02-28 21:23:29 -0300

    include authentication refresh token

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users, only: [:sessions], controllers: {sessions: 'api/v1/sessions'} 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json}, path: "/" do
     namespace :v1, path: "/" do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
