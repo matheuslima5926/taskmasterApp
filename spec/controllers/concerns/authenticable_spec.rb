0197f26001739cd6c03e448c5e85f3d685604194
matheus <matheuslima5926@gmail.com>
2021-03-01 20:15:57 -0300

    include basic-auth rspce tests

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -18,4 +18,29 @@ RSpec.describe Authenticable do
             expect(app_controller.current_user).to eq(user)
         end
     end
+    
+    describe "#authenticate_with_token!" do
+        controller do
+            before_action :authenticate_with_token!
+
+            def restricted_action
+            end
+        end
+        context "no user loged" do
+            before do
+                allow(app_controller).to receive(:current_user).and_return(nil)
+                routes.draw { get 'restricted_action' => 'anonymous#restricted_action' }
+                get :restricted_action
+            end
+
+            it 'returns unauthorized status code 401' do
+                expect(response).to have_http_status(401)
+            end
+
+            it 'returns errors json' do
+                json_body = JSON.parse(response.body, symbolize_names: true)
+                expect(json_body).to have_key(:errors)
+            end
+        end
+    end
 end
